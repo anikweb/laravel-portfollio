966cbb15084e13ec7afac0c78dd9efb47c0f090f
anikweb <aniknandi.it@gmail.com>
2021-07-27 15:31:32 +0600

    testimonial add

--- app/Http/Controllers/TestimonialControllers.php
+++ app/Http/Controllers/TestimonialControllers.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\SiteSettings;
 use App\Models\Testimonial;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class TestimonialControllers extends Controller
 {
@@ -22,6 +24,13 @@ class TestimonialControllers extends Controller
         $testimonial->name = $request->name;
         $testimonial->designation = $request->designation;
         $testimonial->praise = $request->summary;
+        if($request->hasFile('image')){
+            $image = $request->file('image');
+            $newName = Str::slug($request->name).'-'.Str::random(5).'.'.$image->getClientOriginalExtension();
+            $destination = public_path('front/images/testimonial/'.$newName);
+            Image::make($image)->save($destination);
+            $testimonial->image =$newName;
+        }
         $testimonial->save();
         return redirect()->route('testimonialView')->with('success','Testimonial add success');
     }
